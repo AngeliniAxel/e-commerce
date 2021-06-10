0a344afebdb2ee61b92eac48243c8e425ef9b6e8
AngeliniAxel <axel.angelini@gmail.com>
2021-06-10 13:44:46 -0300

    Cleaning project

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import images from './images/images';
-const axios = require('axios');
 
 function App() {
-  const [test, setTest] = useState('');
-
-  useEffect(() => {
-    const fetchTest = async () => {
-      const { data } = await axios.get('/api');
-      console.log(data);
-      setTest(data);
-    };
-    fetchTest();
-  }, []);
-
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>{test && test[0].description}</h1>
-        <img src={images.T_SHIRT_PINK} height='200px' width='200px' />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className='App-link'
-            href='https://reactjs.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux-toolkit.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className='App-link'
-            href='https://react-redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
