758765558bce77bead2f02f1f40a5e7ee678578a
AngeliniAxel <axel.angelini@gmail.com>
2021-06-25 14:44:51 +0200

    Adding Heroku db

--- server/database/db.js
+++ server/database/db.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
-  database: 'ecommerce',
+  database: process.env.DB,
+  connectionString: process.env.DB_URI,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
